939305983d9cabaedf7120d8a456de71801237e3
MDesu <2dpunk3@mail.ru>
2023-10-31 23:55:54 +0800

    Убрал std и иправил ошибку вывода значений карточек и игр. Ошибка при чтении файла, но он работает

--- WorkC.cpp
+++ WorkC.cpp
@@ -7,17 +7,19 @@
 #include <locale>
 #include <regex>
 
+using namespace std;
+
 struct Player {
-    std::string fullName;
-    std::string dateOfBirth;
+    string fullName;
+    string dateOfBirth;
     int gamesPlayed;
     int goalsScored;
     int yellowCards;
     int redCards;
 };
 
-void saveDataToFile(const std::vector<Player>& players, const std::string& filename) {
-    std::ofstream file(filename);
+void saveDataToFile(const vector<Player>& players, const string& filename) {
+    ::ofstream file(filename);
     if (file.is_open()) {
         for (const Player& player : players) {
             file << player.fullName << "," << player.dateOfBirth << "," << player.gamesPlayed
@@ -25,226 +27,236 @@ void saveDataToFile(const std::vector<Player>& players, const std::string& filen
         }
         file.close();
         if (file.good()) {
-            std::cout << "Данные успешно сохранены в файле: " << filename << std::endl;
+            cout << "Данные успешно сохранены в файле: " << filename << endl;
         }
         else {
-            std::cout << "Ошибка при сохранении данных в файл." << std::endl;
+            cout << "Ошибка при сохранении данных в файл." << endl;
         }
     }
     else {
-        std::cout << "Ошибка при открытии файла для записи." << std::endl;
+        cout << "Ошибка при открытии файла для записи." << endl;
     }
 }
 
-void loadDataFromFile(std::vector<Player>& players, const std::string& filename) {
-    std::ifstream file(filename);
+void split(const string& line, vector<string>& tokens, char delimiter) {
+	istringstream iss(line);
+    string token;
+    while (getline(iss, token, delimiter)) {
+        tokens.push_back(token);
+    }
+}
+
+void loadDataFromFile(vector<Player>& players, const string& filename) {
     players.clear();
+    ifstream file(filename);
 
-    if (file.is_open()) {
-        std::string line;
-        while (std::getline(file, line)) {
-            Player player;
-            std::istringstream iss(line);
-            std::string fullName, dateOfBirth;
-            if (std::getline(iss, fullName, ',')) {
-                // Преобразуем имя игрока к нижнему регистру
-                std::transform(fullName.begin(), fullName.end(), fullName.begin(), ::tolower);
-                player.fullName = fullName;
-            }
-            if (std::getline(iss, dateOfBirth, ',')) {
-                player.dateOfBirth = dateOfBirth;
-            }
-            if (iss >> player.gamesPlayed >> player.goalsScored >> player.yellowCards >> player.redCards) {
-                players.push_back(player);
-            }
-            else {
-                // Если считывание не удалось, устанавливаем значения в 0
-                player.gamesPlayed = 0;
-                player.goalsScored = 0;
-                player.yellowCards = 0;
-                player.redCards = 0;
-                players.push_back(player);
-            }
+    if (!file.is_open()) {
+        throw runtime_error("Ошибка при открытии файла для чтения.");
+    }
+
+    string line;
+    while (getline(file, line)) {
+        Player player;
+        vector<string> tokens;
+        split(line, tokens, ',');
+
+        if (tokens.size() >= 2) {
+            player.fullName = tokens[0];
+            transform(player.fullName.begin(), player.fullName.end(), player.fullName.begin(), ::tolower);
+            player.dateOfBirth = tokens[1];
         }
-        file.close();
-        if (file.good()) {
-            std::cout << "Данные успешно загружены из файла: " << filename << std::endl;
+
+        if (tokens.size() >= 6) {
+            player.gamesPlayed = stoi(tokens[2]);
+            player.goalsScored = stoi(tokens[3]);
+            player.yellowCards = stoi(tokens[4]);
+            player.redCards = stoi(tokens[5]);
         }
         else {
-            std::cout << "Ошибка при чтении данных из файла." << std::endl;
+            player.gamesPlayed = 0;
+            player.goalsScored = 0;
+            player.yellowCards = 0;
+            player.redCards = 0;
         }
+
+        players.push_back(player);
     }
-    else {
-        std::cout << "Ошибка при открытии файла для чтения." << std::endl;
+
+    file.close();
+
+    if (!file.good()) {
+        throw runtime_error("Ошибка при чтении данных из файла.");
     }
+
+    cout << "Данные успешно загружены из файла: " << filename << endl;
 }
 
 
 
-void addPlayer(std::vector<Player>& players) {
+void addPlayer(vector<Player>& players) {
     Player player;
-    std::cin.ignore();
-    std::cout << "Введите данные для нового игрока:" << std::endl;
-    std::cout << "Ф.И.О. игрока: ";
-    std::getline(std::cin, player.fullName);
-    std::cout << "Дата рождения: ";
-    std::getline(std::cin, player.dateOfBirth);
-    std::cout << "Количество игр: ";
-    std::cin >> player.gamesPlayed;
-    std::cout << "Количество забитых мячей: ";
-    std::cin >> player.goalsScored;
-    std::cout << "Количество желтых карточек: ";
-    std::cin >> player.yellowCards;
-    std::cout << "Количество красных карточек: ";
-    std::cin >> player.redCards;
+    cin.ignore();
+    cout << "Введите данные для нового игрока:" << endl;
+    cout << "Ф.И.О. игрока: ";
+    getline(cin, player.fullName);
+    cout << "Дата рождения: ";
+    getline(cin, player.dateOfBirth);
+    cout << "Количество игр: ";
+    cin >> player.gamesPlayed;
+    cout << "Количество забитых мячей: ";
+    cin >> player.goalsScored;
+    cout << "Количество желтых карточек: ";
+    cin >> player.yellowCards;
+    cout << "Количество красных карточек: ";
+    cin >> player.redCards;
 
     players.push_back(player);
-    std::cout << "Игрок добавлен." << std::endl;
+    cout << "Игрок добавлен." << endl;
 }
 
-void editPlayer(std::vector<Player>& players) {
+void editPlayer(vector<Player>& players) {
     int index;
-    std::cout << "Введите индекс игрока, которого вы хотите отредактировать: ";
-    std::cin >> index;
+    cout << "Введите индекс игрока, которого вы хотите отредактировать: ";
+    cin >> index;
 
     if (index >= 0 && index < players.size()) {
         Player& player = players[index];
-        std::cin.ignore();
-        std::cout << "Введите новые данные для игрока #" << index << ":" << std::endl;
-        std::cout << "Ф.И.О. игрока: ";
-        std::getline(std::cin, player.fullName);
-        std::cout << "Дата рождения: ";
-        std::getline(std::cin, player.dateOfBirth);
-        std::cout << "Количество игр: ";
-        std::cin >> player.gamesPlayed;
-        std::cout << "Количество забитых мячей: ";
-        std::cin >> player.goalsScored;
-        std::cout << "Количество желтых карточек: ";
-        std::cin >> player.yellowCards;
-        std::cout << "Количество красных карточек: ";
-        std::cin >> player.redCards;
-        std::cout << "Игрок отредактирован." << std::endl;
+        cin.ignore();
+        cout << "Введите новые данные для игрока #" << index << ":" << endl;
+        cout << "Ф.И.О. игрока: ";
+        getline(cin, player.fullName);
+        cout << "Дата рождения: ";
+        getline(cin, player.dateOfBirth);
+        cout << "Количество игр: ";
+        cin >> player.gamesPlayed;
+        cout << "Количество забитых мячей: ";
+        cin >> player.goalsScored;
+        cout << "Количество желтых карточек: ";
+        cin >> player.yellowCards;
+        cout << "Количество красных карточек: ";
+        cin >> player.redCards;
+        cout << "Игрок отредактирован." << endl;
     }
     else {
-        std::cout << "Индекс игрока недопустим." << std::endl;
+        cout << "Индекс игрока недопустим." << endl;
     }
 }
 
-void deletePlayer(std::vector<Player>& players) {
+void deletePlayer(vector<Player>& players) {
     int index;
-    std::cout << "Введите индекс игрока, которого вы хотите удалить: ";
-    std::cin >> index;
+    cout << "Введите индекс игрока, которого вы хотите удалить: ";
+    cin >> index;
 
     if (index >= 0 && index < players.size()) {
         players.erase(players.begin() + index);
-        std::cout << "Игрок удален." << std::endl;
+        cout << "Игрок удален." << endl;
     }
     else {
-        std::cout << "Индекс игрока недопустим." << std::endl;
+        cout << "Индекс игрока недопустим." << endl;
     }
 }
 
-void printPlayers(const std::vector<Player>& players) {
-    std::cout << "Список игроков:" << std::endl;
+void printPlayers(const vector<Player>& players) {
+    cout << "Список игроков:" << endl;
     for (int i = 0; i < players.size(); ++i) {
         const Player& player = players[i];
         int yellowCards = player.yellowCards < 0 ? 0 : player.yellowCards;
         int redCards = player.redCards < 0 ? 0 : player.redCards;
-        std::cout << "Игрок #" << i << ": " << player.fullName << " (Дата рождения: "
+        cout << "Игрок #" << i << ": " << player.fullName << " (Дата рождения: "
             << player.dateOfBirth << ", Забитых мячей: " << player.goalsScored
-            << ", Желтых карточек: " << yellowCards << ", Красных карточек: " << redCards << ")" << std::endl;
+            << ", Желтых карточек: " << yellowCards << ", Красных карточек: " << redCards << ")" << endl;
     }
 }
 
 
 
-void sortPlayers(std::vector<Player>& players, int field) {
+void sortPlayers(vector<Player>& players, int field) {
     switch (field) {
     case 1:
-        std::sort(players.begin(), players.end(), [](const Player& a, const Player& b) {
+        sort(players.begin(), players.end(), [](const Player& a, const Player& b) {
             return a.dateOfBirth < b.dateOfBirth;
             });
         printPlayers(players);
         break;
     case 2:
-        std::sort(players.begin(), players.end(), [](const Player& a, const Player& b) {
+        sort(players.begin(), players.end(), [](const Player& a, const Player& b) {
             return a.gamesPlayed > b.gamesPlayed;
             });
         printPlayers(players);
         break;
     case 3:
-        std::sort(players.begin(), players.end(), [](const Player& a, const Player& b) {
+        sort(players.begin(), players.end(), [](const Player& a, const Player& b) {
             return a.goalsScored > b.goalsScored;
             });
         printPlayers(players);
         break;
     case 4:
-        std::sort(players.begin(), players.end(), [](const Player& a, const Player& b) {
+        sort(players.begin(), players.end(), [](const Player& a, const Player& b) {
             return (a.yellowCards + a.redCards) > (b.yellowCards + b.redCards);
             });
         printPlayers(players);
         break;
     default:
-        std::cout << "Недопустимое поле сортировки." << std::endl;
+        cout << "Недопустимое поле сортировки." << endl;
         break;
     }
 }
 
-void searchPlayers(const std::vector<Player>& players, const std::string& searchTerm) {
-    std::cout << "Результаты поиска:" << std::endl;
+void searchPlayers(const vector<Player>& players, const string& searchTerm) {
+    cout << "Результаты поиска:" << endl;
     for (int i = 0; i < players.size(); ++i) {
         const Player& player = players[i];
-        if (player.fullName.find(searchTerm) != std::string::npos) {
-            std::cout << "Игрок #" << i << ": " << player.fullName << " (Дата рождения: "
-                << player.dateOfBirth << ", Забитых мячей: " << player.goalsScored << ")" << std::endl;
+        if (player.fullName.find(searchTerm) != string::npos) {
+            cout << "Игрок #" << i << ": " << player.fullName << " (Дата рождения: "
+                << player.dateOfBirth << ", Забитых мячей: " << player.goalsScored << ")" << endl;
         }
     }
 }
 
 void displayAbout() {
-    std::cout << "О программе:" << std::endl;
-    std::cout << "Версия программы: 1.0" << std::endl;
-    std::cout << "Дата последних изменений: 2023-10-30" << std::endl;
-    std::cout << "Автор: Ваше имя и контактная информация" << std::endl;
+    cout << "О программе:" << endl;
+    cout << "Версия программы: 1.0" << endl;
+    cout << "Дата последних изменений: 2023-10-30" << endl;
+    cout << "Автор: Ваше имя и контактная информация" << endl;
 }
 
 void displayUserManual() {
-    std::ifstream userManual("user_manual.txt");
+    ifstream userManual("user_manual.txt");
     if (userManual.is_open()) {
-        std::string line;
-        while (std::getline(userManual, line)) {
-            std::cout << line << std::endl;
+        string line;
+        while (getline(userManual, line)) {
+            cout << line << endl;
         }
         userManual.close();
     }
     else {
-        std::cout << "Ошибка при загрузке руководства пользователя." << std::endl;
+        cout << "Ошибка при загрузке руководства пользователя." << endl;
     }
 }
 
 void displayTask() {
-    std::cout << "Задание: ..." << std::endl;
+    cout << "Задание: ..." << endl;
 }
 
-void displayFootballMenu(std::vector<Player>& players, const std::string& filename) {
-    std::string searchTerm;
+void displayFootballMenu(vector<Player>& players, const string& filename) {
+    string searchTerm;
     while (true) {
-        std::cout << "\nМеню \"Работа с футболистами\":" << std::endl;
-        std::cout << "1. Добавить игрока" << std::endl;
-        std::cout << "2. Редактировать игрока" << std::endl;
-        std::cout << "3. Удалить игрока" << std::endl;
-        std::cout << "4. Вывести список игроков" << std::endl;
-        std::cout << "5. Сортировать игроков" << std::endl;
-        std::cout << "6. Поиск игроков" << std::endl;
-        std::cout << "7. Сохранить данные" << std::endl;
-        std::cout << "8. Вернуться в главное меню" << std::endl;
+        cout << "\nМеню \"Работа с футболистами\":" << endl;
+        cout << "1. Добавить игрока" << endl;
+        cout << "2. Редактировать игрока" << endl;
+        cout << "3. Удалить игрока" << endl;
+        cout << "4. Вывести список игроков" << endl;
+        cout << "5. Сортировать игроков" << endl;
+        cout << "6. Поиск игроков" << endl;
+        cout << "7. Сохранить данные" << endl;
+        cout << "8. Вернуться в главное меню" << endl;
 
         int choice;
-        std::cin >> choice;
+        cin >> choice;
         if (!(choice)) {
-            std::cin.clear();  // Сбрасываем флаг ошибки
-            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');  // Очищаем буфер ввода
-            std::cout << "Ошибка ввода. Введите цифру." << std::endl;
+            cin.clear();  // Сбрасываем флаг ошибки
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');  // Очищаем буфер ввода
+            cout << "Ошибка ввода. Введите цифру." << endl;
         }
         else {
             switch (choice) {
@@ -261,19 +273,19 @@ void displayFootballMenu(std::vector<Player>& players, const std::string& filena
                 printPlayers(players);
                 break;
             case 5:
-                std::cout << "Выберите поле для сортировки (1-4):" << std::endl;
-                std::cout << "1. Дата рождения" << std::endl;
-                std::cout << "2. Количество игр" << std::endl;
-                std::cout << "3. Количество забитых мячей" << std::endl;
-                std::cout << "4. Количество карточек" << std::endl;
+                cout << "Выберите поле для сортировки (1-4):" << endl;
+                cout << "1. Дата рождения" << endl;
+                cout << "2. Количество игр" << endl;
+                cout << "3. Количество забитых мячей" << endl;
+                cout << "4. Количество карточек" << endl;
                 int field;
-                std::cin >> field;
+                cin >> field;
                 sortPlayers(players, field);
                 break;
             case 6:
-                std::cin.ignore();
-                std::cout << "Введите строку для поиска: ";
-                std::getline(std::cin, searchTerm);
+                cin.ignore();
+                cout << "Введите строку для поиска: ";
+                getline(cin, searchTerm);
                 searchPlayers(players, searchTerm);
                 break;
             case 7:
@@ -281,10 +293,10 @@ void displayFootballMenu(std::vector<Player>& players, const std::string& filena
                 break;
             case 8:
                 saveDataToFile(players, filename);
-                std::cout << "Данные сохранены. Возвращение в главное меню." << std::endl;
+                cout << "Данные сохранены. Возвращение в главное меню." << endl;
                 return;
             default:
-                std::cout << "Недопустимое действие." << std::endl;
+                cout << "Недопустимое действие." << endl;
                 break;
             }
         }
@@ -292,26 +304,30 @@ void displayFootballMenu(std::vector<Player>& players, const std::string& filena
 }
 
 int main() {
-    std::locale::global(std::locale(""));
-    std::vector<Player> players;
-    std::string filename = "football_players.txt";
-
-    loadDataFromFile(players, filename);
+    locale::global(locale(""));
+    vector<Player> players;
+    const string filename = "players.csv";
+    try {
+        loadDataFromFile(players, filename);
+    }
+    catch (const exception& e) {
+        cerr << "Ошибка тут: " << e.what() << endl;
+    }
 
     while (true) {
-        std::cout << "\nГлавное меню:" << std::endl;
-        std::cout << "1. Работа с футболистами" << std::endl;
-        std::cout << "2. О программе" << std::endl;
-        std::cout << "3. Руководство пользователя" << std::endl;
-        std::cout << "4. Задание" << std::endl;
-        std::cout << "5. Выйти" << std::endl;
+        cout << "\nГлавное меню:" << endl;
+        cout << "1. Работа с футболистами" << endl;
+        cout << "2. О программе" << endl;
+        cout << "3. Руководство пользователя" << endl;
+        cout << "4. Задание" << endl;
+        cout << "5. Выйти" << endl;
 
         int choice;
-        std::cin >> choice;
+        cin >> choice;
         if (!(choice)) {
-            std::cin.clear();  // Сбрасываем флаг ошибки
-            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');  // Очищаем буфер ввода
-            std::cout << "Ошибка ввода. Введите цифру." << std::endl;
+            cin.clear();  // Сбрасываем флаг ошибки
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');  // Очищаем буфер ввода
+            cout << "Ошибка ввода. Введите цифру." << endl;
         }
         else {
             switch (choice) {
@@ -328,10 +344,10 @@ int main() {
                 displayTask();
                 break;
             case 5:
-                std::cout << "Программа завершена." << std::endl;
+                cout << "Программа завершена." << endl;
                 return 0;
             default:
-                std::cout << "Недопустимое действие." << std::endl;
+                cout << "Недопустимое действие." << endl;
                 break;
             }
         }
